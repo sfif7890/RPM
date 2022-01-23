0020c86088278fd62c50a1480af2727bdb5d031c
sfif7890 <98172876+sfif7890@users.noreply.github.com>
2022-01-23 23:59:46 +0400

    Update main.py
    Решение Квадратных уравнений

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-if __name__ == '__main__':
-    print ('Hello World')
+import math
+
+print("ax^2 + bx + c = 0")
+a = int(input("write a "))
+b = int(input("write b "))
+c = int(input("write c "))
+d = (b**2) - (4*a*c)
+otvet = int
+
+def answer():
+    global otvet
+    otvet = ((b*-1)+ d) / (2*a)
+    print("x = ", + otvet)
+
+if d < 0:
+    print("no answer")
+d = math.sqrt(d)
+if d == 0:
+    answer()
+if d > 0:
+    answer()
+    d = d * -1
+    answer()
